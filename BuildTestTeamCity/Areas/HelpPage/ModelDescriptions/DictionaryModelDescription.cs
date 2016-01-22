1336457ea574c46e348017138515cd68132ed5da
Peter Vargovcik <peter.vargovcik@ark-energy.eu>
2016-01-22 08:21:23 +0000

    [TeamCityTest] Initial Commit

---
+++ BuildTestTeamCity/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BuildTestTeamCity.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BuildTestTeamCity/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BuildTestTeamCity.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
